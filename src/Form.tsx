386a058fddb4c09b02f2415f658b76d511822f98
siowhoi <sh.koon@gmail.com>
2024-02-27 09:20:40 +1100

    added simple test

---
+++ src/Form.tsx
@@ -0,0 +1,68 @@
+import { FormEvent, useState, ChangeEvent } from 'react';
+
+export default function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState<null | string>(null);
+  const [status, setStatus] = useState('typing');
+
+  if (status === 'success') {
+    return <h1>That's right!</h1>;
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err: unknown) {
+      setStatus('typing');
+      if (err instanceof Error) {
+        setError(err.message);
+      }
+    }
+  }
+
+  function handleTextareaChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setAnswer(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+          placeholder="Enter city name"
+        />
+        <br />
+        <button
+          disabled={answer.length === 0 || status === 'submitting'}
+          type="submit"
+        >
+          Submit
+        </button>
+        {error !== null && <p className="Error">{error}</p>}
+      </form>
+    </>
+  );
+}
+
+function submitForm(answer: string) {
+  // Pretend it's hitting the network.
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima';
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,12 +1,29 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('demo', () => {
-  expect(true).toBe(true);
-});
-
-test('Renders the main page', () => {
+test('Renders the main page', async () => {
+  const user = userEvent.setup();
   render(<App />);
-  expect(true).toBeTruthy();
+  expect(screen.getByRole('heading')).toHaveTextContent('City quiz');
+
+  const cityField = screen.getByPlaceholderText('Enter city name');
+  await user.type(cityField, 'London');
+
+  await user.click(
+    screen.getByRole('button', {
+      name: 'Submit',
+    }),
+  );
+
+  await waitFor(
+    () => screen.getByText('Good guess but a wrong answer. Try again!'),
+    {
+      timeout: 2000,
+    },
+  );
+  expect(
+    await screen.findByText('Good guess but a wrong answer. Try again!'),
+  ).toBeVisible();
 });
